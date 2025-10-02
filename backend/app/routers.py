c050a62debff9e352c54edeb9debae3aeac2ab6c
nooarche <daniel.delaharpe.golden@gmail.com>
2025-10-02 03:06:31 +0100

    Update routers.py

--- backend/app/routers.py
+++ backend/app/routers.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from typing import List
+from typing import List, Optional
 from datetime import datetime, timedelta, date
 from sqlalchemy import and_
 
